b5885e55c1081f06838389f100a7a5efd8498363
Sriram Suresh <sriram.suresh449@gmail.com>
2022-06-05 11:10:45 -0700

    deploying server to heroku1

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors()); // add this line
+app.use(express.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get("/reply_frontend", function(req, res, next){
+    res.send({message: "sup frontend wyd"});
+});
+
+module.exports = app;
